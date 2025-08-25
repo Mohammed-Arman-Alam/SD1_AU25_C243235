460b277f42d185285c0ad7c75d06c84b72e36794
Arman <armanalam2410@gmail.com>
2025-08-25 15:27:25 +0600

    Add files via upload

---
+++ problem2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+import java.util.*;
+/**
+ *
+ * @author SDLab-PC-CX142
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name  = input.nextLine();
+        System.out.println("your name is "+ name ); 
+    }
+}
