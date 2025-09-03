6e35f2e4953c5e3e0c559f20619406af55899048
MaYa <armanalam2410@gmail.com>
2025-09-03 20:28:00 +0600

    problem 6 solved

---
+++ Problem6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Problem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a Number : ");
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+         System.out.print(i+" ");
+    }
+    }
+}
