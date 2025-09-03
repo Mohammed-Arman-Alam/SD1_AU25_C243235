f973bbc4bf04703861005781df6c9c20ef9e7a19
MaYa <armanalam2410@gmail.com>
2025-09-03 19:43:14 +0600

    updated previous file

---
+++ Problem3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        if(number%2==0){
+            System.out.println(number + " is even number");
+        }
+        else{
+            System.out.println(number + " is odd number");
+        }
+    }
+}
--- problem4.java
+++ problem4.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.mavenproject1;
 import java.util.*;
-/**
- *
- * @author SDLab-PC-CX142
- */
-public class Mavenproject1 {
-
+public class problem4 {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
         
@@ -18,13 +8,13 @@ public class Mavenproject1 {
         int number2 = input.nextInt();
         int number3 = input.nextInt();
         if(number1>number2 && number1>number3){
-            System.out.println(number1 +"is largest");
+            System.out.println(number1 +"is the largest");
         }
         else if(number2>number1 && number2>number3){
-            System.out.println(number2 +"is largest");
+            System.out.println(number2 +" is the largest");
         }
         else{
-            System.out.println(number3 +"is largest");
+            System.out.println(number3 +" is the largest");
         }
     }
 }
